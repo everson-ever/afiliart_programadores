1aee93e803ef2d7bb4948008da20e9240c625038
Everson Silva <eversonsilva9799@gmail.com>
2020-03-06 19:14:59 -0300

    Adicionar rotas restantes do crud de programadores

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,9 @@ import ProgrammerController from '../app/controllers/ProgrammerController';
 const routes = Router();
 
 routes.get('/programadores', ProgrammerController.index);
+routes.get('/programadores/:id', ProgrammerController.show);
 routes.post('/programadores', ProgrammerController.store);
+routes.put('/programadores/:id', ProgrammerController.update);
+routes.delete('/programadores/:id', ProgrammerController.destroy);
 
 export default routes;
