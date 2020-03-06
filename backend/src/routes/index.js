82209b0f9d183b1930acdfc13cc3829652b6ea83
Everson Silva <eversonsilva9799@gmail.com>
2020-03-05 21:59:37 -0300

    Adicinar rota posta do /programadores

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,5 +4,6 @@ import ProgrammerController from '../app/controllers/ProgrammerController';
 const routes = Router();
 
 routes.get('/programadores', ProgrammerController.index);
+routes.post('/programadores', ProgrammerController.store);
 
 export default routes;
