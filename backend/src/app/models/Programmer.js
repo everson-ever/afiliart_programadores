454bf3401e939aee2ab9e6aa00b52b07f73e770a
Everson Silva <eversonsilva9799@gmail.com>
2020-03-06 19:16:11 -0300

    Adicionar parâmetro min nas habilidades

--- backend/src/app/models/Programmer.js
+++ backend/src/app/models/Programmer.js
@@ -1,67 +1,72 @@
 import mongoose from 'mongoose';
 
 const ProgrammerSchema = new mongoose.Schema({
-  nome: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  telefone: {
-    type: String,
-    required: true,
-  },
-  linkedin: {
-    type: String,
-    required: true,
-  },
-  cidade: {
-    type: String,
-    required: true,
-  },
-  estado: {
-    type: String,
-    required: true,
-  },
-  turno: {
-    type: String,
-    enum: ['Manhã', 'Tarde', 'Noite'],
-    required: true,
-  },
-  habilidades: {
-    nodejs: {
-      type: Number,
-      required: true,
-      max: 5,
-      default: 0,
-    },
-    angular: {
-      type: Number,
-      required: true,
-      max: 5,
-      default: 0,
-    },
-    html: {
-      type: Number,
-      required: true,
-      max: 5,
-      default: 0,
-    },
-    css: {
-      type: Number,
-      required: true,
-      max: 5,
-      default: 0,
-    },
-    bancoDados: {
-      type: Number,
-      required: true,
-      max: 5,
-      default: 0,
-    },
-  },
+	nome: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	telefone: {
+		type: String,
+		required: true
+	},
+	linkedin: {
+		type: String,
+		required: true
+	},
+	cidade: {
+		type: String,
+		required: true
+	},
+	estado: {
+		type: String,
+		required: true
+	},
+	turno: {
+		type: String,
+		enum: [ 'Manhã', 'Tarde', 'Noite' ],
+		required: true
+	},
+	habilidades: {
+		nodejs: {
+			type: Number,
+			required: true,
+			min: 0,
+			max: 5,
+			default: 0
+		},
+		angular: {
+			type: Number,
+			required: true,
+			min: 0,
+			max: 5,
+			default: 0
+		},
+		html: {
+			type: Number,
+			required: true,
+			min: 0,
+			max: 5,
+			default: 0
+		},
+		css: {
+			type: Number,
+			required: true,
+			min: 0,
+			max: 5,
+			default: 0
+		},
+		bancoDados: {
+			type: Number,
+			required: true,
+			min: 0,
+			max: 5,
+			default: 0
+		}
+	}
 });
 
 export default mongoose.model('ProgrammerSchema', ProgrammerSchema);
