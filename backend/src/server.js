f2a08003c9799614df9d9e2bee23b78d1e5ea871
Everson Silva <eversonsilva9799@gmail.com>
2020-03-07 14:52:31 -0300

    Organizar estrutura do código

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,30 +4,30 @@ import mongoose from 'mongoose';
 import routes from './routes';
 
 class App {
-	constructor() {
-		this.express = express();
+  constructor() {
+    this.express = express();
 
-		this.middlewares();
-		this.database();
-		this.routes();
-	}
+    this.middlewares();
+    this.database();
+    this.routes();
+  }
 
-	middlewares() {
-		this.express.use(cors());
-		this.express.use(express.json());
-	}
+  middlewares() {
+    this.express.use(cors());
+    this.express.use(express.json());
+  }
 
-	database() {
-		mongoose.connect('mongodb://localhost:27017/afiliart_programadores', {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-			useFindAndModify: false
-		});
-	}
+  database() {
+    mongoose.connect('mongodb://localhost:27017/afiliart_programadores', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+  }
 
-	routes() {
-		this.express.use('/api', routes);
-	}
+  routes() {
+    this.express.use('/api', routes);
+  }
 }
 
 export default new App().express;
