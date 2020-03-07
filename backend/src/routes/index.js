25f6d6f4039f106e5bd2a5462aa6c133a0d13136
Everson Silva <eversonsilva9799@gmail.com>
2020-03-06 21:05:17 -0300

    Adicinar rota para cadastrar e listar usuários do sistema

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import ProgrammerController from '../app/controllers/ProgrammerController';
+import UserController from '../app/controllers/UserController';
 
 const routes = Router();
+
+routes.get('/usuarios', UserController.index);
+routes.post('/usuarios', UserController.store);
 
 routes.get('/programadores', ProgrammerController.index);
 routes.get('/programadores/:id', ProgrammerController.show);
