522c284e29c72b17da4a47ed0e1dd418d6e774b9
Everson Silva <eversonsilva9799@gmail.com>
2020-03-06 22:23:30 -0300

    Adicionar controller de autenticação. Adicionar método de realizar autentição

---
+++ backend/src/app/controllers/AuthController.js
@@ -0,0 +1,30 @@
+import Response from './../models/Response';
+import UserSchema from './../models/User';
+
+class AuthController {
+  async store(req, res) {
+    try {
+      const { email, senha } = req.body;
+
+      const user = await UserSchema.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json(new Response(400, 'credencias incorretas', null));
+      }
+
+      if (!(await user.checkPassword(senha))) {
+        return res
+          .status(400)
+          .json(new Response(400, 'credencias incorretas', null));
+      }
+
+      return res.status(200).json(new Response(200, 'sucesso', user));
+    } catch (err) {
+      return res.status(500).json(new Response(500, 'erro', null));
+    }
+  }
+}
+
+export default new AuthController();
