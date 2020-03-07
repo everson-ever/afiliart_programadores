e64a486945057da1b6b9b7fdac9d41c6c359ea8f
Everson Silva <eversonsilva9799@gmail.com>
2020-03-06 21:32:47 -0300

    Criptografar senha do usuário ao ser salva

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -1,27 +1,36 @@
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-	nome: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true
-	},
-	senha: {
-		type: String,
-		required: true
-	},
-	role: {
-		type: String,
-		default: 'USER',
-		enum: [ 'USER', 'ADMIN' ]
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now()
-	}
+  nome: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  senha: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'USER',
+    enum: ['USER', 'ADMIN'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('senha')) {
+    return next();
+  }
+
+  this.senha = await bcrypt.hash(this.senha, 8);
 });
 
 export default mongoose.model('UserSchema', UserSchema);
