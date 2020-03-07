a22dbde3d9064abaa038f5c1b18641bc5d3ec066
Everson Silva <eversonsilva9799@gmail.com>
2020-03-06 23:33:56 -0300

    Modificar permissões das rotas

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,8 @@ import AuthController from '../app/controllers/AuthController';
 import ProgrammerController from '../app/controllers/ProgrammerController';
 import UserController from '../app/controllers/UserController';
 import auth from '../app/middlewares/auth';
+import permission from './../app/middlewares/permission';
+import roles from './../app/utils/roles';
 
 const routes = Router();
 
@@ -10,14 +12,30 @@ routes.post('/auth', AuthController.store);
 
 routes.use(auth);
 
-routes.get('/usuarios', UserController.index);
-routes.post('/usuarios', UserController.store);
-routes.delete('/usuarios/:id', UserController.destroy);
+routes.get(
+  '/usuarios',
+  permission([roles.admin, roles.gestor]),
+  UserController.index
+);
+routes.post('/usuarios', permission(roles.admin), UserController.store);
+routes.delete('/usuarios/:id', permission(roles.admin), UserController.destroy);
 
 routes.get('/programadores', ProgrammerController.index);
 routes.get('/programadores/:id', ProgrammerController.show);
-routes.post('/programadores', ProgrammerController.store);
-routes.put('/programadores/:id', ProgrammerController.update);
-routes.delete('/programadores/:id', ProgrammerController.destroy);
+routes.post(
+  '/programadores',
+  permission(roles.admin, roles.gestor),
+  ProgrammerController.store
+);
+routes.put(
+  '/programadores/:id',
+  permission(roles.admin, roles.gestor),
+  ProgrammerController.update
+);
+routes.delete(
+  '/programadores/:id',
+  permission(roles.admin, roles.gestor),
+  ProgrammerController.destroy
+);
 
 export default routes;
