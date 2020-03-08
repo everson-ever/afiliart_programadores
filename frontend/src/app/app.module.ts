3c11bed17442bec06368ad82eddb701fca04ca22
Everson Silva <eversonsilva9799@gmail.com>
2020-03-08 15:39:41 -0300

    Configurar angular-jwt

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,21 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  JwtHelperService,
+  JwtModule,
+  JwtModuleOptions
+} from "@auth0/angular-jwt";
+import { ToastrModule, ToastrService } from "ngx-toastr";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
+
+const JWT_Module_Options: JwtModuleOptions = {
+  config: {
+    tokenGetter: () => localStorage.getItem("token")
+  }
+};
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,9 +27,11 @@ import { CoreModule } from "./core/core.module";
     BrowserAnimationsModule,
     CoreModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    JwtModule.forRoot(JWT_Module_Options)
   ],
-  providers: [],
+  providers: [ToastrService, JwtHelperService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
