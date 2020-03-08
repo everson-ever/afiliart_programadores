9100b2f186608f299b65dcc1ef71b54042136fb4
Everson Silva <eversonsilva9799@gmail.com>
2020-03-08 15:42:02 -0300

    Redirecionar de rota raiz para login

--- frontend/src/app/seguranca/seguranca-routing-module.ts
+++ frontend/src/app/seguranca/seguranca-routing-module.ts
@@ -2,7 +2,10 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
 
-const routes: Routes = [{ path: "login", component: LoginComponent }];
+const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
