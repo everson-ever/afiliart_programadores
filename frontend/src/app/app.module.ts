5359a921923f5071829c15e9742f314a6172b658
Everson Silva <eversonsilva9799@gmail.com>
2020-03-08 20:53:20 -0300

    Configurar interceptador

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,38 +1,46 @@
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import {
-  JwtHelperService,
-  JwtModule,
-  JwtModuleOptions
-} from "@auth0/angular-jwt";
-import { ToastrModule, ToastrService } from "ngx-toastr";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { CoreModule } from "./core/core.module";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CoreModule } from './core/core.module';
+import { TokenInterceptor } from './seguranca/token.interceptor';
 
-const JWT_Module_Options: JwtModuleOptions = {
-  config: {
-    tokenGetter: () => localStorage.getItem("token")
-  }
-};
+export function tokenGetter() {
+	return localStorage.getItem('token');
+}
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    CoreModule,
-    FormsModule,
-    HttpClientModule,
-    ToastrModule.forRoot(),
-    JwtModule.forRoot(JWT_Module_Options)
-  ],
-  providers: [ToastrService, JwtHelperService],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, HomeComponent ],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		CoreModule,
+		FormsModule,
+		ToastrModule.forRoot(),
+		HttpClientModule,
+		JwtModule.forRoot({
+			config: {
+				tokenGetter: tokenGetter,
+				whitelistedDomains: [ 'http://localhost:3000/api' ]
+			}
+		})
+	],
+	providers: [
+		ToastrService,
+		JwtHelperService,
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: TokenInterceptor,
+			multi: true
+		}
+	],
+	bootstrap: [ AppComponent ]
 })
 export class AppModule {}
