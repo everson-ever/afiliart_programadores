883d4fab6afbd4d5effb33a19aea83ae29fe17f3
Everson Silva <eversonsilva9799@gmail.com>
2020-03-08 12:40:01 -0300

    Importa serviço do angular-jwt

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { JwtHelperService } from "@auth0/angular-jwt";
 import { MenuModule } from "primeng/menu";
 import { SegurancaModule } from "../seguranca/seguranca.module";
 import { MenuComponent } from "./components/menu/menu.component";
@@ -8,6 +9,7 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 @NgModule({
   declarations: [NavbarComponent, MenuComponent],
   imports: [CommonModule, MenuModule, SegurancaModule],
-  exports: [NavbarComponent, SegurancaModule]
+  exports: [NavbarComponent, SegurancaModule],
+  providers: [JwtHelperService]
 })
 export class CoreModule {}
