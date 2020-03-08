b8484739816a3df47bd3c0a098855a8a9b882755
Everson Silva <eversonsilva9799@gmail.com>
2020-03-08 20:55:42 -0300

    Remover módulos e adicionar o provider TokenInterceptor

--- frontend/src/app/seguranca/seguranca.module.ts
+++ frontend/src/app/seguranca/seguranca.module.ts
@@ -1,22 +1,16 @@
-import { CommonModule } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { ButtonModule } from "primeng/button";
-import { InputTextModule } from "primeng/inputtext";
-import { LoginComponent } from "./components/login/login.component";
-import { SegurancaRoutingModule } from "./seguranca-routing-module";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { LoginComponent } from './components/login/login.component';
+import { SegurancaRoutingModule } from './seguranca-routing-module';
+import { TokenInterceptor } from './token.interceptor';
 
 @NgModule({
-  declarations: [LoginComponent],
-  imports: [
-    CommonModule,
-    SegurancaRoutingModule,
-    ButtonModule,
-    InputTextModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  exports: [LoginComponent]
+	declarations: [ LoginComponent ],
+	imports: [ CommonModule, SegurancaRoutingModule, ButtonModule, InputTextModule, FormsModule ],
+	exports: [ LoginComponent ],
+	providers: [ TokenInterceptor ]
 })
 export class SegurancaModule {}
