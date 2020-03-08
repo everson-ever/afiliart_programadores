1bb9250d2c9ff71188fc7c9d66b2da59f75f7310
Everson Silva <eversonsilva9799@gmail.com>
2020-03-08 20:50:47 -0300

    Interceptador de requisições para adicionar o token

---
+++ frontend/src/app/seguranca/token.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/auth.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+	constructor(public auth: AuthService) {}
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		request = request.clone({
+			setHeaders: {
+				Authorization: `Bearer ${this.auth.getToken()}`
+			}
+		});
+		return next.handle(request);
+	}
+}
