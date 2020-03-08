643bcf40ea5a155b54aed8c42febc6e5f6b60fd9
Everson Silva <eversonsilva9799@gmail.com>
2020-03-08 12:38:08 -0300

    Acessar serviço de segurança

--- frontend/src/app/seguranca/components/login/login.component.ts
+++ frontend/src/app/seguranca/components/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
+  public email: string;
+  public senha: string;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public login(form: NgForm) {
+    this.authService.login(this.email, this.senha);
   }
-
 }
