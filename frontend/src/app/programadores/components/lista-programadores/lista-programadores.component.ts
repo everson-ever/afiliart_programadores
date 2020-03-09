37f16b320ea62d5ebf7c2cc26cf65434749fb25b
Everson Silva <eversonsilva9799@gmail.com>
2020-03-09 19:40:12 -0300

    Implementar listagem de programadores e esconder botões de ações para quem não tem acesso

--- frontend/src/app/programadores/components/lista-programadores/lista-programadores.component.ts
+++ frontend/src/app/programadores/components/lista-programadores/lista-programadores.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ErrorHandlerService } from "src/app/core/services/error-handler.service";
+import { AuthService } from "src/app/seguranca/services/auth.service";
+import { Programador } from "../../models/Programador";
+import { ProgramadoresService } from "../../services/programadores.service";
 
 @Component({
-  selector: 'app-lista-programadores',
-  templateUrl: './lista-programadores.component.html',
-  styleUrls: ['./lista-programadores.component.css']
+  selector: "app-lista-programadores",
+  templateUrl: "./lista-programadores.component.html",
+  styleUrls: ["./lista-programadores.component.css"]
 })
 export class ListaProgramadoresComponent implements OnInit {
+  public programadores: Programador[] = [];
+  public display = false;
+  public habilidade: any;
 
-  constructor() { }
+  constructor(
+    private programadoresService: ProgramadoresService,
+    private errorHandler: ErrorHandlerService,
+    public authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.getProgramadores();
   }
 
+  public showHability(programador) {
+    this.habilidade = programador.habilidades;
+    this.display = true;
+  }
+
+  public getProgramadores() {
+    this.programadoresService
+      .getProgramadores()
+      .then(response => {
+        this.programadores = response.data;
+      })
+      .catch(err => {
+        this.errorHandler.handler(err);
+      });
+  }
 }
--- frontend/src/app/programadores/programadores.module.ts
+++ frontend/src/app/programadores/programadores.module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ButtonModule } from "primeng/button";
+import { DialogModule } from "primeng/dialog";
 import { ListaProgramadoresComponent } from "./components/lista-programadores/lista-programadores.component";
 import { ProgramadoresRoutingModule } from "./programadores-routing-module";
 
 @NgModule({
   declarations: [ListaProgramadoresComponent],
-  imports: [CommonModule, ProgramadoresRoutingModule]
+  imports: [
+    CommonModule,
+    ProgramadoresRoutingModule,
+    ButtonModule,
+    DialogModule
+  ]
 })
 export class ProgramadoresModule {}
