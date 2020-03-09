7b10a1c7b69ead9ea6a3cee099726a136b117f91
Everson Silva <eversonsilva9799@gmail.com>
2020-03-09 20:30:05 -0300

    Método excluir

--- frontend/src/app/programadores/services/programadores.service.ts
+++ frontend/src/app/programadores/services/programadores.service.ts
@@ -1,21 +1,40 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { ApiService } from "src/app/core/services/api.service";
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: "root"
 })
 export class ProgramadoresService {
-	constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private apiService: ApiService) {}
 
-	public getProgramadores(): Promise<any> {
-		return this.http
-			.get('http://localhost:3000/api/programadores')
-			.toPromise()
-			.then((response) => {
-				return Promise.resolve(response);
-			})
-			.catch((err) => {
-				return Promise.reject(err);
-			});
-	}
+  public index(): Promise<any> {
+    return this.http
+      .get(`${this.apiService.url}/programadores`)
+      .toPromise()
+      .then(response => {
+        return Promise.resolve(response);
+      })
+      .catch(err => {
+        return Promise.reject(err);
+      });
+  }
+
+  public destroy(id: number): Promise<any> {
+    return this.http
+      .delete(`${this.apiService.url}/programadores/${id}`)
+      .toPromise()
+      .then(response => {
+        console.log(response);
+        return Promise.resolve(response);
+      })
+      .catch(err => {
+        if (err.status === 400) {
+          return Promise.reject("Não foi possível deletar");
+        }
+        if (err.status === 404) {
+          return Promise.reject("Recurso já não existe na base de dados");
+        }
+      });
+  }
 }
