41858c344751a35cabc558b6184c8b2cf6a17737
Everson Silva <eversonsilva9799@gmail.com>
2020-03-09 20:29:33 -0300

    Alterar nome da propriedade id

--- frontend/src/app/programadores/models/Programador.ts
+++ frontend/src/app/programadores/models/Programador.ts
@@ -1,11 +1,11 @@
 export class Programador {
-	id: string;
-	nome: string;
-	email: string;
-	telefone: string;
-	linkedin: string;
-	cidade: string;
-	estado: string;
-	turno: string;
-	habilidades: object;
+  _id: string;
+  nome: string;
+  email: string;
+  telefone: string;
+  linkedin: string;
+  cidade: string;
+  estado: string;
+  turno: string;
+  habilidades: object;
 }
