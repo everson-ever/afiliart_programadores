e8355f2346b50adc08314cce3eb489af8d5cc3f9
Everson Silva <eversonsilva9799@gmail.com>
2020-03-09 21:40:14 -0300

    Implementar logout

--- frontend/src/app/seguranca/services/auth.service.ts
+++ frontend/src/app/seguranca/services/auth.service.ts
@@ -1,50 +1,60 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
-	providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
-	public jwtPayload: any;
+  public jwtPayload: any;
 
-	constructor(private http: HttpClient, private jwtHelperService: JwtHelperService) {
-		this.reloadToken();
-	}
+  constructor(
+    private http: HttpClient,
+    private jwtHelperService: JwtHelperService,
+    private router: Router
+  ) {
+    this.reloadToken();
+  }
 
-	public login(email: string, senha: string): Promise<void> {
-		return this.http
-			.post('http://localhost:3000/api/auth', { email, senha })
-			.toPromise()
-			.then((response: any) => {
-				this.storeToken(response.data.token);
-			})
-			.catch((err) => {
-				if (err.status === 400) {
-					if (err.error.message === 'credencias incorretas') {
-						return Promise.reject('E-mail ou senha incorretos');
-					}
-				}
+  public login(email: string, senha: string): Promise<void> {
+    return this.http
+      .post("http://localhost:3000/api/auth", { email, senha })
+      .toPromise()
+      .then((response: any) => {
+        this.storeToken(response.data.token);
+      })
+      .catch(err => {
+        if (err.status === 400) {
+          if (err.error.message === "credencias incorretas") {
+            return Promise.reject("E-mail ou senha incorretos");
+          }
+        }
 
-				return Promise.reject(err);
-			});
-	}
+        return Promise.reject(err);
+      });
+  }
 
-	public storeToken(token: string) {
-		this.jwtPayload = this.jwtHelperService.decodeToken(token);
+  public logout() {
+    localStorage.removeItem("token");
+    this.router.navigate(["/login"]);
+  }
 
-		localStorage.setItem('token', token);
-	}
+  public storeToken(token: string) {
+    this.jwtPayload = this.jwtHelperService.decodeToken(token);
 
-	public reloadToken() {
-		const token = localStorage.getItem('token');
+    localStorage.setItem("token", token);
+  }
 
-		if (token) {
-			this.storeToken(token);
-		}
-	}
+  public reloadToken() {
+    const token = localStorage.getItem("token");
 
-	public getToken() {
-		return localStorage.getItem('token');
-	}
+    if (token) {
+      this.storeToken(token);
+    }
+  }
+
+  public getToken() {
+    return localStorage.getItem("token");
+  }
 }
