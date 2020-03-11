e2e683d488adf999c9a9da4f3275ece7a8213ca2
Everson Silva <eversonsilva9799@gmail.com>
2020-03-10 22:20:18 -0300

    Adicionar dependência

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,14 +1,15 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { ProgramadoresModule } from "../programadores/programadores.module";
-import { SegurancaModule } from "../seguranca/seguranca.module";
-import { MenuComponent } from "./components/menu/menu.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProgramadoresModule } from '../programadores/programadores.module';
+import { SegurancaModule } from '../seguranca/seguranca.module';
+import { MenuComponent } from './components/menu/menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [NavbarComponent, MenuComponent],
-  imports: [CommonModule, SegurancaModule],
-  exports: [NavbarComponent, SegurancaModule, ProgramadoresModule],
-  providers: []
+	declarations: [ NavbarComponent, MenuComponent ],
+	imports: [ CommonModule, SegurancaModule, RouterModule ],
+	exports: [ NavbarComponent, SegurancaModule, ProgramadoresModule ],
+	providers: []
 })
 export class CoreModule {}
