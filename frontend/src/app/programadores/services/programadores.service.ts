891f56eb22b273edb748ae1aa0825ac32dd6c989
Everson Silva <eversonsilva9799@gmail.com>
2020-03-10 22:14:35 -0300

    Criar método store para cadastrar novo programador

--- frontend/src/app/programadores/services/programadores.service.ts
+++ frontend/src/app/programadores/services/programadores.service.ts
@@ -1,40 +1,53 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { ApiService } from "src/app/core/services/api.service";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ApiService } from 'src/app/core/services/api.service';
+import { Programador } from '../models/Programador';
 
 @Injectable({
-  providedIn: "root"
+	providedIn: 'root'
 })
 export class ProgramadoresService {
-  constructor(private http: HttpClient, private apiService: ApiService) {}
+	constructor(private http: HttpClient, private apiService: ApiService) {}
 
-  public index(): Promise<any> {
-    return this.http
-      .get(`${this.apiService.url}/programadores`)
-      .toPromise()
-      .then(response => {
-        return Promise.resolve(response);
-      })
-      .catch(err => {
-        return Promise.reject(err);
-      });
-  }
+	public index(): Promise<any> {
+		return this.http
+			.get(`${this.apiService.url}/programadores`)
+			.toPromise()
+			.then((response) => {
+				return Promise.resolve(response);
+			})
+			.catch((err) => {
+				return Promise.reject(err);
+			});
+	}
 
-  public destroy(id: number): Promise<any> {
-    return this.http
-      .delete(`${this.apiService.url}/programadores/${id}`)
-      .toPromise()
-      .then(response => {
-        console.log(response);
-        return Promise.resolve(response);
-      })
-      .catch(err => {
-        if (err.status === 400) {
-          return Promise.reject("Não foi possível deletar");
-        }
-        if (err.status === 404) {
-          return Promise.reject("Recurso já não existe na base de dados");
-        }
-      });
-  }
+	public destroy(id: number): Promise<any> {
+		return this.http
+			.delete(`${this.apiService.url}/programadores/${id}`)
+			.toPromise()
+			.then((response) => {
+				console.log(response);
+				return Promise.resolve(response);
+			})
+			.catch((err) => {
+				if (err.status === 400) {
+					return Promise.reject('Não foi possível deletar');
+				}
+				if (err.status === 404) {
+					return Promise.reject('Recurso já não existe na base de dados');
+				}
+			});
+	}
+
+	public store(programador: Programador): Promise<any> {
+		return this.http
+			.post(`${this.apiService.url}/programadores`, programador)
+			.toPromise()
+			.then((response) => {
+				return Promise.resolve(response);
+			})
+			.catch((err) => {
+				return Promise.reject(err);
+			});
+	}
 }
