56f1881693110f31a8ed73fcb499096fcb4ff0fc
Everson Silva <eversonsilva9799@gmail.com>
2020-03-10 22:18:12 -0300

    Adicionar rota de cadastro de programadores

--- frontend/src/app/programadores/programadores-routing-module.ts
+++ frontend/src/app/programadores/programadores-routing-module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { ListaProgramadoresComponent } from "./components/lista-programadores/lista-programadores.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastrarProgramadorComponent } from './components/cadastrar-programador/cadastrar-programador.component';
+import { ListaProgramadoresComponent } from './components/lista-programadores/lista-programadores.component';
 
 const routes: Routes = [
-  { path: "programadores", component: ListaProgramadoresComponent }
+	{ path: 'programadores', component: ListaProgramadoresComponent },
+	{ path: 'programadores/cadastrar', component: CadastrarProgramadorComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forChild(routes) ],
+	exports: [ RouterModule ]
 })
 export class ProgramadoresRoutingModule {}
