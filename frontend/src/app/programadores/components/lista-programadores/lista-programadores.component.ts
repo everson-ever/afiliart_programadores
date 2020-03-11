b78202fe2afe6bab4e3443fd8cc630dca0d3fd1d
Everson Silva <eversonsilva9799@gmail.com>
2020-03-10 22:17:24 -0300

    Redirecionar para a rota cadastrar

--- frontend/src/app/programadores/components/lista-programadores/lista-programadores.component.ts
+++ frontend/src/app/programadores/components/lista-programadores/lista-programadores.component.ts
@@ -1,57 +1,63 @@
-import { Component, OnInit } from "@angular/core";
-import { ErrorHandlerService } from "src/app/core/services/error-handler.service";
-import { AuthService } from "src/app/seguranca/services/auth.service";
-import { Programador } from "../../models/Programador";
-import { ProgramadoresService } from "../../services/programadores.service";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ErrorHandlerService } from 'src/app/core/services/error-handler.service';
+import { AuthService } from 'src/app/seguranca/services/auth.service';
+import { Programador } from '../../models/Programador';
+import { ProgramadoresService } from '../../services/programadores.service';
 
 @Component({
-  selector: "app-lista-programadores",
-  templateUrl: "./lista-programadores.component.html",
-  styleUrls: ["./lista-programadores.component.css"]
+	selector: 'app-lista-programadores',
+	templateUrl: './lista-programadores.component.html',
+	styleUrls: [ './lista-programadores.component.css' ]
 })
 export class ListaProgramadoresComponent implements OnInit {
-  public programadores: Programador[] = [];
-  public display = false;
-  public habilidade: any;
+	public programadores: Programador[] = [];
+	public display = false;
+	public habilidade: any;
 
-  constructor(
-    private programadoresService: ProgramadoresService,
-    private errorHandler: ErrorHandlerService,
-    public authService: AuthService
-  ) {}
+	constructor(
+		private programadoresService: ProgramadoresService,
+		private errorHandler: ErrorHandlerService,
+		public authService: AuthService,
+		private router: Router
+	) {}
 
-  ngOnInit() {
-    this.index();
-  }
+	ngOnInit() {
+		this.index();
+	}
 
-  public showHability(programador) {
-    this.habilidade = programador.habilidades;
-    this.display = true;
-  }
+	cadastrar() {
+		this.router.navigate([ '/programadores/cadastrar' ]);
+	}
 
-  public index() {
-    this.programadoresService
-      .index()
-      .then(response => {
-        this.programadores = response.data;
-      })
-      .catch(err => {
-        this.errorHandler.handler(err);
-      });
-  }
+	public showHability(programador) {
+		this.habilidade = programador.habilidades;
+		this.display = true;
+	}
 
-  public destroy(id: number) {
-    this.programadoresService
-      .destroy(id)
-      .then(response => {
-        this.errorHandler.handlerSuccess("Deletado com sucesso");
+	public index() {
+		this.programadoresService
+			.index()
+			.then((response) => {
+				this.programadores = response.data;
+			})
+			.catch((err) => {
+				this.errorHandler.handler(err);
+			});
+	}
 
-        this.programadores = this.programadores.filter(programador => {
-          return programador._id.toString() !== id.toString();
-        });
-      })
-      .catch(err => {
-        this.errorHandler.handler(err);
-      });
-  }
+	public destroy(id: number) {
+		this.programadoresService
+			.destroy(id)
+			.then((response) => {
+				this.errorHandler.handlerSuccess('Deletado com sucesso');
+
+				this.programadores = this.programadores.filter((programador) => {
+					return programador._id.toString() !== id.toString();
+				});
+			})
+			.catch((err) => {
+				this.errorHandler.handler(err);
+			});
+	}
 }
